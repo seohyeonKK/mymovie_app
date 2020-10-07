90d90dfa843bcd93edd58514c54d7e5829d65b15
김서현 <62590535+seohyeonKK@users.noreply.github.com>
2020-10-08 02:41:23 +0900

    Set propTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
@@ -11,41 +12,58 @@ const foodILike = [
     id: 2,
     name: "Samgyeopsal",
     image: "https://t1.daumcdn.net/cfile/tistory/9942B3395A3501C304",
+    rating: 4.9,
   },
   {
     id: 3,
     name: "Bibimbap",
     image:
       "https://recipe1.ezmember.co.kr/cache/recipe/2017/04/13/abcf293fb7d0d73d61e274127ced7b931.jpg",
+    rating: 4.8,
   },
   {
     id: 4,
     name: "Doncasu",
     image:
       "https://th3.tmon.kr/thumbs/image/6ea/082/d57/2906e06a0_700x700_95_FIT.jpg",
+    rating: 5.5,
   },
   {
     id: 5,
     name: "Kimbap",
     image:
       "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
+    rating: 4.7,
   },
 ];
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h2>I like {name}</h2>
       <img src={picture} alt={name} />
+      <h4>{rating}/5.0</h4>
     </div>
+    //alt : 시각장애인을 위한
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
+
 function App() {
   return (
-    <div className="App">
+    <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
