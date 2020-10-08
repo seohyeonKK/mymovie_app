2daa1662ed528c598384ef8d1970ab9c8d54697b
김서현 <62590535+seohyeonKK@users.noreply.github.com>
2020-10-08 22:34:04 +0900

    Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,82 +1,45 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg",
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image: "https://t1.daumcdn.net/cfile/tistory/9942B3395A3501C304",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2017/04/13/abcf293fb7d0d73d61e274127ced7b931.jpg",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://th3.tmon.kr/thumbs/image/6ea/082/d57/2906e06a0_700x700_95_FIT.jpg",
-    rating: 5.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
-    rating: 4.7,
-  },
-];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
 
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name} />
-      <h4>{rating}/5.0</h4>
-    </div>
-    //alt : 시각장애인을 위한
-  );
+  //바꿀 데이터를 state에
+  state = {
+    count: 0,
+  };
+  add = () => {
+    this.setState((current) => ({ count: current.count + 1 }));
+  };
+
+  minus = () => {
+    this.setState((current) => ({ count: current.count - 1 }));
+  };
+
+  componentDidMount() {
+    console.log("componenet rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Goodbye, cruel world");
+  }
+
+  render() {
+    console.log("I'm rendering");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-// (dish) => (
-//   <Food name={dish.name} />
-// )
-
-//Same Thing
-
-// function (dish) {
-//   return <Food name={dish.name} />;
-// }
 
 export default App;
