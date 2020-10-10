b404e13be612de56e1329aff153075a06be1bd34
김서현 <62590535+seohyeonKK@users.noreply.github.com>
2020-10-10 21:07:08 +0900

    Navigation and Redirecting

--- src/App.js
+++ src/App.js
@@ -2,24 +2,17 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
+import "./App.css";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
-      {/* exact는 / 에서만! 다른 데서는 nope */}
       <Route path="/about" component={About} />
-      {/* 렌더링 할 스크린, 뭘할지 정해주는 props */}
-
-      {/* <Route path="/home">
-        <h1>Home</h1>
-      </Route>
-      <Route path="/home/introduction">
-        <h1>Introduction</h1>
-      </Route>
-      <Route path="/about">
-        <h1>About</h1>
-      </Route> */}
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import propTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ year, title, summary, poster, genres }) {
   return (
-    <div className="movie">
-      <img src={poster} alt={title} title={title}></img>
-      <div className="movie_data">
-        <h3 className="movie_title">{title}</h3>
-        <h5 className="movie_year">{year}</h5>
-        <ul className="movie_genres">
-          {genres.map((genre, index) => (
-            <li key={index} className="genres_genre">
-              {genre}
-            </li>
-          ))}
-        </ul>
-        <p className="movie_summary">{summary}</p>
+    <Link
+      to={{
+        pathname: "/movie-detail",
+        state: {
+          year: year,
+          title: title,
+          summary: summary,
+          poster,
+          genres,
+        },
+      }}
+    >
+      <div className="movie">
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie_data">
+          <h3 className="movie_title">{title}</h3>
+          <h5 className="movie_year">{year}</h5>
+          <ul className="movie_genres">
+            {genres.map((genre, index) => (
+              <li key={index} className="genres_genre">
+                {genre}
+              </li>
+            ))}
+          </ul>
+          <p className="movie_summary">{summary}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
